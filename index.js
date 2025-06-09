a232b09aed0fe3db2841996ed10f8af61065e3dc
Damexxa <166002332+Damexxa@users.noreply.github.com>
2025-06-09 20:17:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+  AOS.init();
+
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const progressBars = document.querySelectorAll('.progress');
+
+    progressBars.forEach(bar => {
+      const width = bar.style.width;
+      bar.style.width = '0';
+      setTimeout(() => {
+        bar.style.width = width;
+      }, 100); // Delay to trigger animation
+    });
+  });
+
+
+function sendMail() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        
+    }
+
+     emailjs
+     
+     .send("service_dqszwvj","template_1jgzvvr",parms)
+     
+     .then(() => {
+                alert("Your Message has been sent Successfully")
+            })
+            .catch((error) => {
+                console.log("error sending email:", error)
+                alert("failed to send mail")
+            })
+     
+     
+     
+     
+    
+}
